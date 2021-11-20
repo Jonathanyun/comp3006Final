f897e2ab2768504802868cb13e4c07e16fdf89b7
spooknewt <93363599+spooknewt@users.noreply.github.com>
2021-11-20 14:36:40 -0600

    Added Class
    Updated code to include map making class

--- ufo_covid_analysis.py
+++ ufo_covid_analysis.py
@@ -1,21 +1,3 @@
-#  _,~-'
-#    \ \
-#     \ \  ,                     .-"""-.
-#     _,~-'\                   /        \
-#    '\     \                 /_        _\
-#      \     \               // \     / \ \
-#       \     \             | \__\   /__/ |
-#        \_,~-'\            \     ||     /
-#         \_,~-'\            \          /
-#          \_,~-'\             \  __  /
-#           \_,~-'              '.__.'
-#               \
-#                \
-#                 \
-#                  \
-#                   \
-
-
 #Python Libraries
 import argparse
 import sys
@@ -28,91 +10,138 @@ import ufo_data as ufo
 import covid_vaccinations as covid
 import lat_lon as ll
 
-
-#Creates parser for command line arguements
-parser = argparse.ArgumentParser(description="Parse arguements to filter and combine both data sets")
-parser.add_argument('-c', '--city', action='store_true', dest = "ufo_state", help="Flags if UFO data is mapped by city or by state")
-parser.add_argument('-s', '--shape', choices=['cylinder', 'light', 'circle', 'sphere', 'disk', 'fireball',
-                                              'unknown', 'oval', 'other', 'cigar', 'rectangle', 'chevron',
-                                              'triangle', 'formation', 'N/A', 'delta', 'changing', 'egg',
-                                              'diamond', 'flash', 'teardrop', 'cone', 'cross', 'pyramid',
-                                              'round', 'crescent', 'flare', 'hexagon', 'dome', 'changed',
-                                              'all'], dest='ufo_shape', type=str, help="Filters UFO type")
-parser.add_argument('-v','--vaccine',)
-parser.add_argument('-t','--time',)
-
-#Color for map inputs
-parser.add_argument('-p','--palette', choices=['BuGn', 'BuPu', 'GnBu', 'OrRd', 'PuBu', 'PuBuGn', 'PuRd', 
-                                               'RdPu', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd'],
-                    nargs=2, default=['PuRd','BuGn'], dest="colors", help="Picks the Color palette for the map")
-parser.add_argument('-l','--location',type=str,dest = "state")
-args = parser.parse_args()
-
-## Insert & Clean UFO Data
-#Filtering UFO data based off command line arguements and ufo_data module functions
-if ("-c" in str(sys.argv)):
-    ufo_df = ufo.ufo_data_clean("ufo_sighting_data.csv","state_lat_long.csv", False)
-    ufo_df = ufo.ufo_counts(ufo_df,True)
-else:
-    ufo_df = ufo.ufo_data_clean("ufo_sighting_data.csv","state_lat_long.csv", True)
-    ufo_df = ufo.ufo_counts(ufo_df,False)
+class map_maker:
+    def __init__(self):
+        ''' Module initializes class
+        '''
+        #Creates Map and adds data to it
+        get_lat_lon()
+        build_basemap()
+        add_ufo_data()
+        add_vaccine_data()
     
-if ("-s" in str(sys.argv)) and (args.ufo_shape.lower() != 'all'):
-    ufo_df = ufo.ufo_type(ufo_df,args.ufo_shape.lower())
-
-ufo_df = ufo.shape_counts(ufo_df)
-#UFO Plots
-ufo_plt_1 = plt.figure(1)
-cluster_plt = ufo.shape_plot(ufo_df,False)
-plt.title("3 Cluster Plot For K-Values of UFO Type and UFOs per State")
-ufo_plt_2 = plt.figure(2)
-plt.figure(figsize=(12, 4))
-bar_plt = ufo.state_plot(ufo_df)
-plt.xticks(rotation=90)
-plt.title("UFO Sightings Per State")
-plt.show()
-
-## Instert & Clean Vaccine Data
-#Filtering Vaccine data based off command line arguements and ___ module functions
-
-
-## Map building with Folium
-#latitude and longitude variables for folium map
-#add latitude & longitude chooser
-lat, lon = ll.get_lat_lon(str(args.state),"state_lat_long.csv")
-#Creates Map
-map_usa = fl.Map(location=[lat,lon],zoom_start=3)
-#UFO map data section
-if ("-c" in str(sys.argv)):
-    #Creates Heat Map overlay on built map of UFO information
-    heat_data = [[row['latitude'],row['longitude']] for index, row in ufo_df.iterrows()]
-    HeatMap(heat_data, name="UFOs",min_opacity=0, radius=20,blur=0).add_to(map_usa)
-else:
-    #Creates Choropleth over on built map for the USA
-    #builds state lines
-    url = (
-        "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data"
-    )
-    state_geo = f"{url}/us-states.json"
-
-
-    fl.Choropleth(
+    def get_lat_lon(self,state_input = str(args.state), state_filename = "state_lat_long.csv"):
+        ''' Method gets the latitude and longitude for the lat and lon variables from command line input
+            using the lat_lon module
+            inputs:
+            state_input: string, state requested in command line
+            state_filename: string, .csv file with state and longitude/latitude data
+            outputs:
+            self.lat: float, latitude variable
+            self.lon: float, longitude variable
+        '''
+        #intializes self.state_filename and self.state_input
+        self.state_filename = state_filename
+        self.state_input = state_input
+        #Creates self.lat and self.lon using lat_lon module
+        self.lat, self.lon = ll.get_lat_lon(self.state_input,self.state_filename)
+        return self.lat, self.lon
     
-          # geographical locations
-        geo_data = state_geo,                     
-        name = "test",
+    def build_basemap(self):
+        ''' Method creates map data and imports state lines for choropleth plot
+            output:
+            self.usa_map: map data used in folium maps
+        '''
+        #uses the folium map function and specified starting latitude and longitude to create map data
+        self.usa_map = fl.Map(location=[self.lat,self.lon],zoom_start=3)
+        #imports state lines
+        self.url = (
+            "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data"
+            )
+        self.state_geo = f"{self.url}/us-states.json"
+        return self.usa_map
     
-          # the data set we are using
-        data = ufo_df,                        
-        columns = ["state_upper", "ufo_count"],
+    def add_ufo_data(self,city_arg = args.ufo_state,color_arg = args.colors[0], ufo_data=ufo_df):
+        ''' Method adds UFO layer to map object. UFO layer is either a heatmap or a choropleth plot.
+            inputs:
+            city_arg: Boolean, whether or not ufo data is by city  or state
+            color_arg: str, if plot is choropleth, changes the color scheme
+            ufo_data: dataframe, ufo data used to make map layer
+        '''
+        #initializes variables related to ufo_data and command line inputs
+        self.city_arg = city_arg
+        self.color_arg = color_arg
+        self.ufo_data = ufo_data
+        
+        #Changes type of map layer created depending on city_arg variable
+        if self.city_arg == True:
+            #creates heatmap from ufo data
+            self.ufo_heat = [[row['latitude'],row['longitude']] for index, row in self.ufo_data.iterrows()]
+            HeatMap(self.ufo_heat, name="UFOs",min_opacity=0, radius=20,blur=0).add_to(self.usa_map)
+        else:
+            #changes ufo_data state abbreiviations to capital to match data from us-states.json
+            self.ufo_data["state/province"] = self.ufo_data["state/province"].str.upper()
+            #creates Choropleth
+            fl.Choropleth(
+                #adds state lines and names map layer
+                geo_data = self.state_geo,                     
+                name = "UFOs",
+                #uses the state abbreviations and ufo count data for plot
+                data = self.ufo_data,                        
+                columns = ["state/province", "ufo_count"],
+                #changes plot color based of command input
+                fill_color = str(self.city_arg),                      
+                fill_opacity = 0.7,
+                line_opacity = .1,
+                  key_on = "feature.id",
+                legend_name = "UFO Sighting Rate (%)",
+            ).add_to(self.usa_map)
+        return self.usa_map
     
-          # YlGn refers to yellow and green
-        fill_color = str(args.colors[0]),                      
-        fill_opacity = 0.7,
-        line_opacity = .1,
-          key_on = "feature.id",
-        legend_name = "UFO Sighting Rate (%)",
-    ).add_to(map_usa)     
+    def add_vaccine_data(self,):
+        return
     
+def main():
+    #Creates parser for command line arguements
+    parser = argparse.ArgumentParser(description="Parse arguements to filter and combine both data sets")
+    parser.add_argument('-c', '--city', action='store_true', dest = "ufo_state", help="Flags if UFO data is mapped by city or by state")
+    parser.add_argument('-s', '--shape', choices=['cylinder', 'light', 'circle', 'sphere', 'disk', 'fireball',
+                                                  'unknown', 'oval', 'other', 'cigar', 'rectangle', 'chevron',
+                                                  'triangle', 'formation', 'N/A', 'delta', 'changing', 'egg',
+                                                  'diamond', 'flash', 'teardrop', 'cone', 'cross', 'pyramid',
+                                                  'round', 'crescent', 'flare', 'hexagon', 'dome', 'changed',
+                                                  'all'], dest='ufo_shape', type=str, help="Filters UFO type")
+    parser.add_argument('-v','--vaccine',)
+    parser.add_argument('-t','--time',)
+    parser.add_argument('-p','--palette', choices=['BuGn', 'BuPu', 'GnBu', 'OrRd', 'PuBu', 'PuBuGn', 'PuRd', 
+                                                   'RdPu', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd'],
+                        nargs=2, default=['PuRd','BuGn'], dest="colors", help="Picks the Color palette for the map")
+    parser.add_argument('-l','--location',type=str,dest = "state")
+    args = parser.parse_args()
+    
+    ## Insert & Clean UFO Data
+    #Filtering UFO data based off command line arguements and ufo_data module functions
+    if args.ufo_state == True:
+        ufo_df = ufo.ufo_data_clean("ufo_sighting_data.csv","state_lat_long.csv", False)
+        ufo_df = ufo.ufo_counts(ufo_df,True)
+    else:
+        ufo_df = ufo.ufo_data_clean("ufo_sighting_data.csv","state_lat_long.csv", True)
+        ufo_df = ufo.ufo_counts(ufo_df,False)
 
-#combine data in one map
+    if ("-s" in str(sys.argv)) and (args.ufo_shape.lower() != 'all'):
+        ufo_df = ufo.ufo_type(ufo_df,args.ufo_shape.lower())
+    #Adds count of ufo shapes seen, will return same value for every row if a shape is selected in command line
+    ufo_df = ufo.shape_counts(ufo_df)
+    
+    ## UFO Plots
+    #K-Value 3 Cluster Plot for UFO data, ignores shape input to see relationship between UFO Shape Sightings
+    #and sightings per location
+    ufo_plt_1 = plt.figure(1)
+    cluster_plt = ufo.shape_plot(ufo_df,args.ufo_state)
+    plt.title("3 Cluster Plot For K-Values of UFO Type and UFOs per State")
+    #Plot of UFO Sightings per state Bar Chart, ignores -c input data filter because that would be too many bars
+    ufo_plt_2 = plt.figure(2)
+    plt.figure(figsize=(12, 4))
+    bar_plt = ufo.state_plot(ufo_df)
+    plt.xticks(rotation=90)
+    plt.title("UFO Sightings Per State")
+    plt.show()
+    
+    ## Vaccine Stuff -
+    
+    #Show combined map
+    Map_made = map_maker()
+    Map_made.usa_map
+    
+if __name__ == '__main__':
+    main()
